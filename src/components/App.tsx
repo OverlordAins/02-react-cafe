26959ad4a11ae28f789575826341bf869b23df36
Daniil <stifflingdagger@gmail.com>
2025-12-23 22:37:17 +0200

    add components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
 // import viteLogo from '/vite.svg'
-import "./App.css";
+import '../App.css';
+
+import Product from './Product';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,9 +18,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React + TS</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -28,6 +30,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Product />
     </>
   );
 }
