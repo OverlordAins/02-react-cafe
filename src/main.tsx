994649d88b29e87244365efa6a746fefb4c697f8
Daniil <stifflingdagger@gmail.com>
2025-12-31 02:00:55 +0200

    cafe-ready

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-
-import App from './components/App.tsx';
-// import Product from './components/Product.tsx';
+import './basic.css';
+import App from './components/App/App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
